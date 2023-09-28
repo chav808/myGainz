6fae27f45bd652b14e9792b5e40229089d47656b
IRODR1502020 <irodr150@fiu.edu>
2023-09-27 21:34:50 -0400

    firebase auth

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import {
+  Typography,
+  TextField,
+  Button,
+  Container,
+  Tab,
+  Tabs,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  InputAdornment,
+  IconButton,
+  Box
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../config/firebase";
+import { minHeight } from "@mui/system";
+
+const HomePage = () => {
+	
+	return (
+		<div>
+			Hello World!
+		</div>
+	);
+};
+
+export default HomePage;
