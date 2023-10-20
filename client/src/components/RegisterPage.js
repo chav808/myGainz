e331f301ca52d4c087fc015449b7272afa44b3e1
IRODR1502020 <irodr150@fiu.edu>
2023-10-20 17:02:03 -0400

    add RegistrationPage
    npm updates and created RegistrationPage

---
+++ client/src/components/RegisterPage.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { Container, Typography, TextField, Button, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/mygainz_logo.png";
+
+const RegisterPage = () => {
+	
+	
+	return (
+	<div>
+		Hello World
+	
+	</div>
+	);
+};
+
+export default RegisterPage;
