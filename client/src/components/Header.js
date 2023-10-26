5b37599af93608d775061f8f28004bb09f69a5c0
IRODR1502020 <irodr150@fiu.edu>
2023-10-26 16:54:38 -0400

    add Header
    started working on header

---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Typography,
+  Container,
+  Tabs,
+  Tab,
+  TextField,
+  Link,
+  Avatar,
+  Box,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import logo from "../assets/mygainz_logo.png";
+import background from "../assets/background.jpg";
+
+const Header = () => {
+	const logoContainerStyle = {
+		
+	};
+	
+	const logoStyle = {
+		
+	};
+	
+	const headerStyle = {
+		backgroundImage: 'url(${background})',
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "cover"
+	};
+	
+	return (
+		<div>
+			Hello from the Header!
+		</div>
+	
+	);
+};
+
+export default Header;
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -21,13 +21,17 @@ import { db } from "../config/firebase";
 import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 import { storage } from "../config/firebase";
 import { minHeight } from "@mui/system";
+import Header from "./Header";
 
 const HomePage = () => {
 	
 	return (
+	<>
+		<Header />
 		<div>
 			Hello World!
 		</div>
+	</>
 	);
 };
 
