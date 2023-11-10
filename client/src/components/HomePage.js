81b1e5baa277e2856f40d81b48226b5185f08b12
IRODR1502020 <irodr150@fiu.edu>
2023-11-09 19:46:01 -0500

    added workout plan page
    ana work integrated, initial styling done

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -22,15 +22,71 @@ import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 import { storage } from "../config/firebase";
 import { minHeight } from "@mui/system";
 import Header from "./Header";
+import '../scss/HomePage.scss';
 
 const HomePage = () => {
 	
 	return (
 	<>
 		<Header />
-		<div>
-			Hello World!
+			<body>
+		<h1>MyGainz Dashboard</h1>
+		<p>View your weekly workout plan, make adjustments and browse our list of exercises</p>
+		<div class="dashboard"> 
+			
+			<h2>My Weekly Workouts</h2>
+			<a href="generateWorkout.html">
+			<button type="workoutGenerator">Generate New Workout</button>
+			</a>
+			<a href="workoutList.html">
+				<button type="workoutList">View all Exercises</button>
+			</a>
+			<div class="workout_list">
+				<div class="weekday">
+					<h3>Monday</h3>
+					<p>Exercises for today:</p>
+					<li>Squats</li> 
+					<li>Leg extensions</li>
+					<li>Bulgarian Split Squats</li>
+				</div>
+				<div class="weekday">
+					<h3>Tuesday</h3>
+					<p>Exercises for today:</p>
+					<li>Bench Press</li>
+					<li>Cable Flies</li>
+					<li>Shoulder Press</li>    
+				</div>
+				<div class="weekday">
+					<h3>Wednesday</h3>
+					<p>Exercises for today:</p>
+					<li>Rest Day</li>
+				</div>
+				<div class="weekday">
+					<h3>Thursday</h3>
+					<p>Exercises for today:</p>
+					<li>Lat Pulldowns</li>
+					<li>Hammer Curls</li>
+					<li>Seated Row</li>
+				</div>
+				<div class="weekday">
+					<h3>Friday</h3>
+					<p>Exercises for today:</p>
+					<li>Full body workout</li>
+				</div>
+				<div class="weekday">
+					<h3>Saturday</h3>
+					<p>Exercises for today:</p>
+					<li>Rest Day</li>
+				</div>
+				<div class="weekday">
+					<h3>Sunday</h3>
+					<p>Exercises for today:</p>
+					<li>Rest Day</li>
+				</div>
+			</div>
 		</div>
+	
+	</body>
 	</>
 	);
 };
