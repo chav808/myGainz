ccead1c572efae91b6aaa9251051f8d8adb9bc28
amtorres28 <anatorressan@gmail.com>
2023-11-20 10:02:01 -0500

    ana push 2

---
+++ SEII/app.js
@@ -0,0 +1,14 @@
+var mondayWorkout = []
+var tuesdayWorkout = []
+var wednesdayWorkout = []
+var thursdayWprkout = []
+var fridayWorkout = []
+var saturdayWorkout = []
+var sundayWorkout = []
+
+if (document.getElementById('Strength').checked && document.getElementById('two').checked) {
+    mondayWorkout = ["Barbell Squat", "Deadlift"]
+    tuesdayWorkout = ["No workout for today!"]
+}
+
+
